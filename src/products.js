14f4548ca1c4b32e6a1a23f36fc9c2ae32723c7c
Amelie Ren <meilou@hotmail.fr>
2020-08-04 11:09:30 +0200

    First push

---
+++ src/products.js
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    id: "whale",
+    name: "Whale Cake",
+    price: 20,
+    image: "whale.jpg",
+  },
+  {
+    id: "reindeer",
+    name: "Reindeer Cake",
+    price: 20,
+    image: "reindeer.jpg",
+  },
+  {
+    id: "unicorn",
+    name: "Unicorn Cake",
+    price: 20,
+    image: "unicorn.jpg",
+  },
+  {
+    id: "panda",
+    name: "Panda Cake",
+    price: 16,
+    image: "panda.jpg",
+  },
+  {
+    id: "cupcake",
+    name: "Cupcake Cake",
+    price: 20,
+    image: "cupcake.jpg",
+  },
+  {
+    id: "avocado",
+    name: "Avocado Cake",
+    price: 16,
+    image: "avocado.jpg",
+  },
+  {
+    id: "mermaid",
+    name: "Mermaid Cake",
+    price: 24,
+    image: "mermaid.jpg",
+  },
+  {
+    id: "onepiece",
+    name: "OnePiece Cake",
+    price: 24,
+    image: "onepiece.jpg",
+  },
+  {
+    id: "bear",
+    name: "Bear Cake",
+    price: 16,
+    image: "bear.jpg",
+  },
+]
