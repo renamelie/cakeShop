f175c02a7f16c4a941a3e65ec3ba72cb3d6f7658
Amelie Ren <meilou@hotmail.fr>
2020-08-06 22:55:47 +0200

    add form

---
+++ src/components/ContactForm.js
@@ -0,0 +1,160 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+import Styled from 'styled-components'
+import { colors, pxToRem } from '../theme/helpers'
+
+const ContactForm = ({ className }) => {
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [message, setMessage] = useState('')
+
+	const onNameChange = e => {
+		setName(e.target.value)
+	}
+
+	const onEmailChange = e => {
+		setEmail(e.target.value)
+	}
+
+	const onMessageChange = e => {
+		setMessage(e.target.value)
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault()
+
+		axios({
+			method: 'POST',
+			url: 'https://cakeshopper.netlify.app/send',
+			data: { name, email, message },
+		}).then(response => {
+			if (response.data.status === 'success') {
+				alert('Message Sent.')
+				this.resetForm()
+			} else if (response.data.status === 'fail') {
+				alert('Message failed to send.')
+			}
+		})
+	}
+
+	const resetForm = () => {
+		setName('')
+		setEmail('')
+		setMessage('')
+	}
+
+	return (
+		<form
+			className={className}
+			onSubmit={handleSubmit.bind(this)}
+			name="Contact Form"
+			method="POST"
+			data-netlify="true"
+		>
+			<input type="hidden" name="form-name" value="Contact Form" />
+			<div className="formGroup">
+				<label htmlFor="email">
+					<input
+						className="form-control"
+						type="text"
+						placeholder="Name"
+						value={name}
+						onChange={onNameChange.bind(this)}
+					/>
+				</label>
+			</div>
+			<div className="formGroup">
+				<label htmlFor="message">
+					<input
+						className="form-control"
+						type="email"
+						placeholder="Email"
+						aria-describedby="emailHelp"
+						value={email}
+						onChange={onEmailChange.bind(this)}
+					/>
+				</label>
+			</div>
+			<div className="formGroup">
+				<textarea
+					className="form-control"
+					placeholder="Message"
+					rows="5"
+					value={message}
+					onChange={onMessageChange.bind(this)}
+				/>
+			</div>
+			<div className="buttonForm">
+				<button type="submit" className="btn btn-primary">
+					Envoyer
+				</button>
+			</div>
+		</form>
+	)
+}
+
+export default Styled(ContactForm)`
+	min-width: 300px;
+	width: 80%;
+	background-color: ${({ theme }) => theme.backgroundColorContact};
+	padding: ${pxToRem(20)};
+	border-radius: ${pxToRem(20)};
+
+	textarea, input {
+		min-width: 300px;
+		background-color: ${colors.darkGrey};
+		padding: ${pxToRem(10)};
+	}
+
+	textarea:focus, input:focus {
+		box-shadow: 0 0 5px 2px ${colors.accentPlus};
+	}
+
+	textarea {
+		resize : none;
+		height: 200px;
+	}
+
+	.formGroup, input {
+		border-radius: ${pxToRem(10)};
+
+	}
+
+	.formGroup > *, input,  .buttonForm > * {
+		border-color: transparent;
+		border-radius: ${pxToRem(10)};
+		transition: 0.5s;
+	}
+
+	.formGroup > *, input {
+		font-family: 'Montserrat', sans-serif;
+		color: ${colors.black};
+		background-color: ${colors.white};
+		width: 100%;
+		outline: none;
+	}
+
+	.formGroup, .buttonForm {
+		display: flex;
+		justify-content: center;
+		margin: ${pxToRem(14)};
+		border-radius: ${pxToRem(10)};
+	}
+
+	button {
+		cursor: pointer;
+		color: ${colors.accent};
+		background-color: ${colors.white};
+		width: ${pxToRem(100)};
+		padding: ${pxToRem(10)};
+		border-radius: ${pxToRem(20)};
+		text-transform: uppercase;
+		outline: none;
+	}
+
+	button:hover {
+		color: ${colors.black};
+		background-color: ${({ theme }) => theme.frontColor};
+	}
+`
--- src/pages/infosContact.js
+++ src/pages/infosContact.js
@@ -2,27 +2,13 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import ContactForm from '../components/ContactForm'
 
 const InfosContactPage = () => (
 	<Layout>
 		<SEO title="Infos Contact Page" />
 		<h1>Informations & contact</h1>
-		<form name="Contact Form" method="POST" data-netlify="true">
-			<input type="hidden" name="form-name" value="Contact Form" />
-			<div>
-				<label htmlFor="email">
-					Your Email:
-					<input type="email" name="email" id="email" />
-				</label>
-			</div>
-			<div>
-				<label htmlFor="message">
-					Message:
-					<textarea name="message" id="message" />
-				</label>
-			</div>
-			<button type="submit">Send</button>
-		</form>
+		<ContactForm />
 	</Layout>
 )
 
