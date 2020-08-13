8dd902831a5915a785fe7b2d8611cd58ffd5e719
Amelie Ren <meilou@hotmail.fr>
2020-08-13 21:12:05 +0200

    modif favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
 				background_color: `#663399`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `src/images/cake-slice.png`, // This path is relative to the root of the site.
+				icon: `src/images/candy.png`, // This path is relative to the root of the site.
 			},
 		},
 		{
